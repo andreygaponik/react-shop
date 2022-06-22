5ce35b2083d3fb520cc6e710c157b6722af9a1de
andreygaponik <andreygaponik@gmail.com>
2022-06-23 00:06:09 +0300

    test

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,7 +3,7 @@ const CartItem = (props) => {
     <>
       <li className="collection-item">
         <div>
-          {props.displayName} x <i className="material-icons cart-quantity" onClick={() => props.decQuantity(props.id)}>remove</i> {props.quantity} <i className="material-icons cart-quantity" onClick={() => props.incQuantity(props.id)}>add</i> = {props.price * props.quantity} rub.
+          {props.displayName} <i className="material-icons cart-quantity" onClick={() => props.decQuantity(props.id)}>remove</i> x {props.quantity} <i className="material-icons cart-quantity" onClick={() => props.incQuantity(props.id)}>add</i> = {props.price * props.quantity} rub.
           <span
             onClick={props.removeFromCart}
             className="secondary-content delete-item">
